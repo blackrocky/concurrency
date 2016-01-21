a568ab4e48f2358b98f075be17cea7c340c3349b
blackrocky <robbie.martinus@yahoo.com.au>
2016-01-21 17:01:42 +1100

    Added countdownlatch example

--- src/main/java/concurrencypractice/countdownlatch/Loan.java
+++ src/main/java/concurrencypractice/countdownlatch/Loan.java
@@ -5,9 +5,12 @@ import concurrencypractice.countdownlatch.checkers.impl.CreditChecker;
 import concurrencypractice.countdownlatch.checkers.impl.IncomeChecker;
 import concurrencypractice.countdownlatch.checkers.impl.RaisedInterestRateChecker;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 import static java.util.Arrays.asList;
 import static java.util.concurrent.Executors.newFixedThreadPool;
@@ -23,13 +26,30 @@ public class Loan {
         final List<AbstractChecker> checkers = asList(creditChecker, incomeChecker, raisedInterestRateChecker);
         final ExecutorService executorService = newFixedThreadPool(checkers.size());
 
+        boolean approved = true;
+        final Set<Future<Boolean>> futures = new HashSet<>();
+
         for (final AbstractChecker checker : checkers) {
-            executorService.submit(checker);
+            final Future<Boolean> submit = executorService.submit(checker);
+            futures.add(submit);
+        }
+
+        for (final Future<Boolean> future : futures) {
+            try {
+                final Boolean result = future.get();
+                if (result != Boolean.TRUE) {
+                    approved = false;
+                    break;
+                }
+            } catch (Exception ex) {
+                approved = false;
+                break;
+            }
         }
 
         countDownLatch.await();
 
-        System.out.println("Loan approved");
+        System.out.println("Loan approved? " + approved);
 
         executorService.shutdown();
     }
